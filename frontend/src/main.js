2bae621a2f5d62c150ba1d68f4abea5c31eb845b
Dominik Paľo <dominik.palo@student.tuke.sk>
2023-11-07 21:15:31 +0100

    feat: added tailwindcss

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue';
 import router from './router'; // Make sure this is pointing to the file where you are exporting the router
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import './assets/styles.css';
+
 // Create a new Vue application and use the router
 const app = createApp(App);
 app.use(VueAxios, axios);
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.html', './src/**/*.vue', './src/**/*.jsx'],
+  // ... rest of the config
+}
+
+
+
